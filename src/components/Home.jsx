3856a623c5725a1e1783ca542146bb97f72e238f
jovanlekic12 <lekicjovan1212@gmail.com>
2024-12-05 00:00:50 +0100

    add style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import Navbar from "./Navbar";
+import Input from "./reusable/Input";
 
 function Home() {
   return (
     <main className="main__container">
       <Navbar></Navbar>
+      <div>
+        <h1>The best pizza .</h1>
+        <h2>Straight out of the oven, straight to you .</h2>
+      </div>
+      <form className="homepage__form">
+        <p>👋🏼 Welcome! Please start telling us your name:</p>
+        <Input placeholder="Your full name" type="text" />
+      </form>
     </main>
   );
 }
