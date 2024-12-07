06af3d6795b78ca4084daebaace529fb682f9b07
jovanlekic12 <lekicjovan1212@gmail.com>
2024-12-07 12:59:10 +0100

    refactor code

--- src/feature/menu/menuSlice.js
+++ src/feature/menu/menuSlice.js
@@ -7,11 +7,19 @@ const initialState = {
 
 const url = "https://react-fast-pizza-api.onrender.com/api/menu";
 
-export const getMenuItems = fetch(url);
+export const getMenuItems = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const menuSlice = createSlice({
   name: "menu",
   initialState,
+  reducers: {},
 });
 
 export default menuSlice.reducer;
