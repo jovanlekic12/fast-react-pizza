ea1d3c63e7bec887f0d26b1d72158f7ecd8bf361
jovanlekic12 <lekicjovan1212@gmail.com>
2024-12-21 22:26:41 +0100

    refactor code

--- src/components/order/OrderListItem.jsx
+++ src/components/order/OrderListItem.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 function OrderItem(props) {
   const { quantity, name, totalPrice, pizzaId } = props;
+  const [ingredients, setIngredients] = useState();
+  const { menuItems } = useSelector((store) => store.global);
+
+  useEffect(() => {
+    const pizza = menuItems.find((item) => item.id === pizzaId);
+    setIngredients(pizza.ingredients);
+  }, []);
 
   return (
     <li className="order__li">
@@ -8,7 +18,9 @@ function OrderItem(props) {
           <p>{quantity}x</p>
           <p>{name}</p>
         </div>
-        <p className="order__li__ingredients">tomato</p>
+        <p className="order__li__ingredients">
+          {ingredients && ingredients.join(", ")}
+        </p>
       </div>
       <h5>${totalPrice}</h5>
     </li>
